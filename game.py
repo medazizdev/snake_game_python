6598e6d7078b07d21065cec69cb590576a3cddd2
medazizdev <mhmdazizbenmessaoud@gmail.com>
2022-04-29 02:24:56 +0200

    Add snake movement algorithm [not the best one -- see comments in the change_snake_body_coordinates function to know more]

--- game.py
+++ game.py
@@ -20,21 +20,36 @@ class Snake():
 	def __init__(self):
 		self.x = 100
 		self.y = 100
-		self.width = 10
-		self.parts = [[self.x, self.y]]
+		self.width = 5
+		self.parts = [[self.x, self.y], [self.x-self.width, self.y], [self.x-2*self.width, self.y], [self.x-3*self.width, self.y], [self.x-4*self.width, self.y], [self.x-5*self.width, self.y], [self.x-6*self.width, self.y]]
 		self.color = GREEN
 		self.direction = 'right'
 		self.velocity = 5
+		print(f'new head coordinate [{self.x}, {self.y}]')
+		print(f'\t{self.parts}\n')
 
-	def move_snake(self):
+	def change_snake_head_coordinates(self):
 		if self.direction == 'right':
 			self.x += self.velocity
 		elif self.direction == 'left':
 			self.x -= self.velocity
-		elif self.direction == 'top':
+		elif self.direction == 'up':
 			self.y -= self.velocity
-		elif self.direction == 'buttom':
+		elif self.direction == 'down':
 			self.y += self.velocity
+
+		print(f'\nnew head coordinate [{self.x}, {self.y}]')
+		
+
+		return [self.x, self.y]
+
+	def change_snake_body_coordinates(self):
+
+		# this solution is not the best -- the velocity should always have the same value as the snake single part width --
+		self.parts.pop(-1)
+		self.parts.insert(0, self.change_snake_head_coordinates())
+		print(f'\t{self.parts}\n')
+		
 
 	def draw_snake(self):
 		for part in self.parts:
@@ -51,6 +66,7 @@ run = True
 
 def update_display():
 	SCREEN.fill(GREY)
+	snake.change_snake_body_coordinates()
 	snake.draw_snake()
 	pygame.display.update()
 
@@ -71,6 +87,19 @@ while run:
 			run = False
 			pygame.quit()
 			sys.exit()
+
+		# game controles
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_RIGHT:
+				snake.direction = 'right'
+			if event.key == pygame.K_LEFT:
+				snake.direction = 'left'
+			if event.key == pygame.K_UP:
+				snake.direction = 'up'
+			if event.key == pygame.K_DOWN:
+				snake.direction = 'down'
+
 
 	# update screen
 
